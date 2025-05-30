78410ccea87e94387aa136cde2cfc9e8ec6ca848
Yvonne JIN <55522464+Yvonne27Jin@users.noreply.github.com>
2025-05-30 19:44:29 +0200

    add launch function

--- R/run_app.R
+++ R/run_app.R
@@ -1,7 +1,7 @@
 #' Run the Shiny Application
 #'
 #' @export
-run_app <- function() {
+launch_sentimentplot <- function() {
   appDir <- system.file("shiny-examples", "my_app", package = "sentimentplots")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `sentimentplots`.", call. = FALSE)
